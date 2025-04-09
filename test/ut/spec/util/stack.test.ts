f9338c207bf46d1cc9994c40a1566027f9679c1d
Steven Cobb <steven@chronosphere.io>
2025-04-09 11:55:27 -0400

    debug test setup for calculatePercentStack

--- test/ut/spec/util/stack.test.ts
+++ test/ut/spec/util/stack.test.ts
@@ -25,6 +25,7 @@ import SeriesModel from '@/src/model/Series';
 import SeriesData from '@/src/data/SeriesData';
 import { createSourceFromSeriesDataOption } from '@/src/data/Source';
 import prepareSeriesDataSchema from '@/src/data/helper/createDimensions';
+import SeriesDimensionDefine from '@/src/data/SeriesDimensionDefine';
 
 function createMockSeriesDataFromSchema(dimensions: string[], rows: (number | null)[][]): SeriesData {
     const hostModel = new Model();
@@ -36,6 +37,19 @@ function createMockSeriesDataFromSchema(dimensions: string[], rows: (number | nu
     const schema = prepareSeriesDataSchema(source, {
         coordDimensions: dimensions.map((name) => ({ name })),
     });
+    const storeDimStart = schema.dimensions.length;
+    schema.dimensions.push(new SeriesDimensionDefine({
+        name: '__stack_result__',
+        type: 'float',
+        isCalculationCoord: true,
+        storeDimIndex: storeDimStart
+    }));
+    schema.dimensions.push(new SeriesDimensionDefine({
+        name: '__stacked_over__',
+        type: 'float',
+        isCalculationCoord: true,
+        storeDimIndex: storeDimStart + 1
+    }));
     const seriesData = new SeriesData(schema, hostModel);
     seriesData.initData(source);
     return seriesData;
@@ -43,8 +57,10 @@ function createMockSeriesDataFromSchema(dimensions: string[], rows: (number | nu
 
 function createMockStackInfo(name: string, values: number[][]): StackInfo {
     const data = createMockSeriesDataFromSchema(['x', 'y'], values);
-    const dimInfo = data.getDimensionInfo('y');
-    dimInfo.isCalculationCoord = true;
+    const yInfo = data.getDimensionInfo('y');
+    if (yInfo) {
+        yInfo.isCalculationCoord = true;
+    }
     data.setCalculationInfo({
         stackedDimension: 'y',
         stackedByDimension: '__stack_by__',
@@ -52,7 +68,13 @@ function createMockStackInfo(name: string, values: number[][]): StackInfo {
         stackedOverDimension: '__stacked_over__',
         isStackedByIndex: true,
     });
-    const seriesModel = { name, getData: () => data } as SeriesModel;
+    // data.getDimensionInfo('y').isCalculationCoord = true;
+    // data.getDimensionInfo('__stack_result__').isCalculationCoord = true;
+    // data.getDimensionInfo('__stacked_over__').isCalculationCoord = true;
+    const seriesModel = {
+        name,
+        getData: () => data
+    } as SeriesModel;
     return {
         data,
         seriesModel,
@@ -66,22 +88,41 @@ function createMockStackInfo(name: string, values: number[][]): StackInfo {
 
 describe('util/stack', function () {
     describe('calculatePercentStack', function () {
-    it('should calculate percent stack', function () {
-        const stackInfoList: StackInfo[] = [
-            createMockStackInfo('a', [
-              [0, 10],
-              [1, 20],
-              [2, 30],
-            ]),
-            createMockStackInfo('b', [
-              [0, 40],
-              [1, 20],
-              [2, 10],
-            ]),
-          ];
+        it('should calculate percent stack', function () {
+            const stackInfoList: StackInfo[] = [
+                createMockStackInfo('a', [
+                [0, 10],
+                [1, 20],
+                [2, 30],
+                ]),
+                createMockStackInfo('b', [
+                [0, 40],
+                [1, 20],
+                [2, 10],
+                ]),
+            ];
 
-        calculatePercentStack(stackInfoList);
-        console.log('FINAL -> stackInfoList: ', stackInfoList);
-    });
+            calculatePercentStack(stackInfoList);
+            const firstSeriesData = stackInfoList[0].data;
+            // const stackResultDim = firstSeriesData.getCalculationInfo('stackResultDimension');
+            // const stackedDimIdx = stackResultDim && firstSeriesData.getDimensionIndex(stackResultDim);
+            const stackedOverDim = firstSeriesData.getCalculationInfo('stackedOverDimension');
+            const stackedOverDimIdx = stackedOverDim && firstSeriesData.getDimensionIndex(stackedOverDim);
+            const firstSeriesDataStore = firstSeriesData.getStore();
+            // console.log('FINAL -> firstSeriesDataStore: ', firstSeriesDataStore);
+            const dataIndex = 1;
+            const stackStartValue = firstSeriesDataStore.get(stackedOverDimIdx, dataIndex);
+            console.log('FINAL -> stackStartValue: ', stackStartValue);
+            // console.log('FINAL -> stackInfoList: ', stackInfoList);
+            // console.log(stackInfoList[0].data);
+            // console.log(stackInfoList[0].data.getValues(0));
+            // console.log(stackInfoList[0].data.getCalculationInfo('stackResultDimension')); // __stack_result__
+            // const getValues = (info: StackInfo) => info.data.mapArray(['__stack_result__'], (val: number[]) => val[0]);
+            // expect(getValues(stackInfoList[0])).toEqual([
+            //     10 / (10 + 40),
+            //     20 / (20 + 20),
+            //     30 / (30 + 10),
+            // ]);
+        });
     });
 });
